9ae01221e1ddd97b8694ad2d4441671656994fee
jolthegreat <katoshin0805@gmail.com>
2023-05-15 22:05:27 +0900

    Initial commit

---
+++ src/main/java/post/chain/chainpostbot/ChainPost.java
@@ -0,0 +1,13 @@
+package post.chain.chainpostbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChainPostBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChainPostBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/post/chain/chainpostbot/ChainPostTests.java
@@ -0,0 +1,13 @@
+package post.chain.chainpostbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChainPostBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
